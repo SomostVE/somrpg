9182f7233624b3763812b1ae174cc3cd83d8af14
SomostVE <66364264+SomostVE@users.noreply.github.com>
2026-08-21 22:00:36 +0200

    feat: route 404 and 500 errors to safe feedback

--- somrpg/urls.py
+++ somrpg/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+handler404 = "game.error_views.page_not_found"
+handler500 = "game.error_views.server_error"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("town/", include("classic.urls")),
